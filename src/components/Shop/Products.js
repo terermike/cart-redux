8c3932029ba6e429354c867dc21d79363f98ec4a
Terer <terermike3@gmail.com>
2024-06-26 23:40:42 +0300

    Implemented addition and removal of items from cart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+    price: 29.99,
+    description: "This is the first product",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+    price: 59.99,
+    description: "This is the second product",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+    price: 19.99,
+    description: "This is the third product",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.key}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+};
+
 const cartSlice = createSlice({
   name: "cart",
-  initialState: { items: [], totalQuantity: 0 },
+  initialState: initialCartState,
   reducers: {
-    addItemToCart(state, action) {
+    addItemsToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          iemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -22,7 +28,8 @@ const cartSlice = createSlice({
     },
     removeitemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find((item) => (item.id = id));
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
