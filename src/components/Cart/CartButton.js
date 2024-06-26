97fed22ae7e6448d7c4edc33d6d7e4c3b84f2bc4
Terer <terermike3@gmail.com>
2024-06-26 11:52:14 +0300

    Modified the files to utilize the ui-slice for conditional rendering of the cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
 import classes from "./CartButton.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  function handleShowCart() {
+    dispatch(cartActions.toggle());
+  }
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={handleShowCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cart";
+import uiSlice from "./ui-slice";
 
-const store = configureStore({ reducer: { cart: cartReducer } });
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
 
 export default store;
